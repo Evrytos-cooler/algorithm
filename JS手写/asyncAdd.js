1fcfef7da4e70dc8142afe6848add65d21931c77
chenzikai <chenzikai.evrytos@bytedance.com>
2025-03-05 14:10:42 +0800

    perf: 探索asyncAdd的更优写法

--- JS手写/asyncAdd.js
+++ JS手写/asyncAdd.js
@@ -2,7 +2,7 @@
 const asyncAdd = (a, b, cb) => {
 	setTimeout(() => {
 		cb(null, a + b)
-	}, 1000)
+	}, 1000 * Math.random())
 }
 
 // 同步做法
@@ -23,7 +23,9 @@ const asyncSum = async (...arr) => {
 	return temp
 }
 
-// asyncSum(1, 2, 3).then(res => console.log(res))
+const now0 = Date.now()
+console.log(await asyncSum(1, 1, 1, 1, 1, 1, 1))
+console.log(Date.now() - now0)
 
 // 由于加法是无序的，所以可以同时执行多个promise
 // 使用递归，每次执行一轮，每一轮用两个数字计算，完成后再次执行一轮
@@ -65,3 +67,57 @@ console.log(Date.now() - now)
 // 我们在每一个Promise落定后去看当前temp中是否有内容，如果没有内容则跳过，因为每两个promise才回去执行下一个计算
 // 如果当前temp有两个元素则继续执行递归内容
 // 每一次落定都执行增加一个count，如果count达到了最终需要的计算次数，就返回
+// 但是由于操作数组的问题，shift()的事件复杂度比较高，所以时间上不如第二个方法
+// 而且还可能遇到 Process exited with code 13 的问题，不知道为什么
+
+const perfConcurrentAsyncSum = async (...arr) => {
+	return new Promise(allResolve => {
+		if (!arr.length) return 0
+		if (arr.length === 1) return arr[0]
+
+		// 如果数组长度为奇数，添加一个 0
+		if (arr.length % 2 === 1) {
+			arr.push(0)
+		}
+
+		// 计算一共需要执行多少次
+		let totalCount = 0
+		let n = arr.length
+		while (n > 1) {
+			n /= 2
+			totalCount += n
+		}
+
+		const sendPromise = () =>
+			new Promise(resolve => {
+				totalCount--
+				asyncAdd(arr.pop(), arr.pop(), (_, sum) => {
+					resolve(sum)
+				})
+			})
+				.then(sum => {
+					if (totalCount === 0) {
+						allResolve(sum)
+					} else {
+						arr.unshift(sum)
+						if (arr.length > 1) {
+							// 继续执行
+							sendPromise()
+						}
+					}
+				})
+				.catch(e => {
+					console.log(e)
+				})
+
+		// 分成两两一组，计算每组的和
+		const length = arr.length
+		for (let i = 0; i < length; i += 2) {
+			sendPromise()
+		}
+	})
+}
+
+const now2 = Date.now()
+console.log(await perfConcurrentAsyncSum(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1))
+console.log(Date.now() - now2)
