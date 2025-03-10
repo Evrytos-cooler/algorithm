3ed23780e68dfa72bc7a24d8d80d2404ebb46ac0
chenzikai <chenzikai.evrytos@bytedance.com>
2025-03-10 13:34:47 +0800

    feat: 未完成，max Rectangle & validParentheses

---
+++ 回溯/longestValidParentheses.js
@@ -0,0 +1,12 @@
+// 回溯暴搜
+const longestValidParentheses = function (s) {
+	s = s.split('')
+	const result = []
+	const traceBack = temp => {
+		// 结束情况 && 处理结果
+		if (!s.length) {
+			result.push(temp.join(''))
+		} else {
+		}
+	}
+}
