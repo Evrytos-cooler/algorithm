e3bb7a272fb55e0baada6315bfe24d3e44931c44
Evrytos <3174232122@qq.com>
2025-03-31 00:46:49 +0800

    feat: deepCopy & shallowCopy

--- JS手写/TODOcopy.js
+++ JS手写/TODOcopy.js
@@ -11,6 +11,11 @@ export const shallowCopy = obj => {
 // 保存特殊类型
 // 处理循环引用
 // 只取本身属性
+// 步骤
+// 1. 处理原始类型（直接返回）
+// 2. 处理特殊object拷贝(Date,func,Reg,set,map,weakset,weakmap, 使用new XXX(obj))
+// 3. 处理循环引用
+// 4. 迭代处理内容 object / Array
 export const deepCopy = (obj, map = new WeakMap()) => {
 	//如果是基础类型 ，返回obj
 	//如果是特殊类型，创建他的对应对象 (Date,RegExp,Array,Map,Set)
@@ -18,18 +23,19 @@ export const deepCopy = (obj, map = new WeakMap()) => {
 	//for in 遍历 obj
 	//判断是否是自有属性
 	//对自有属性递归调用deepCoye
+	if (map.has(obj)) return map.get(obj)
 	let copy = {}
 	if (typeof obj !== 'object' || !obj) return obj
 	if (obj instanceof Array) copy = []
 	if (obj instanceof Date) copy = new Date(obj)
 	if (obj instanceof RegExp) copy = new RegExp(obj)
-	if (obj instanceof Function) copy = new Function(obj)
-	if (obj instanceof Set) copy = new Set(obj)
-	if (obj instanceof Map) copy = new Map(obj)
-	if (obj instanceof WeakMap) copy = new WeakMap(obj)
-	if (obj instanceof WeakSet) copy = new WeakSet(obj)
+	if (obj instanceof Function) copy = obj // function 直接引用？ 保留作用域和闭包
+	if (obj instanceof Set) copy = new Set(obj) // 这里需要继续遍历
+	if (obj instanceof Map) copy = new Map(obj) // 这里需要继续遍历
+	if (obj instanceof WeakMap) copy = new WeakMap(obj) // 这里需要继续遍历
+	if (obj instanceof WeakSet) copy = new WeakSet(obj) // 这里需要继续遍历
 	if (!map.has(obj)) {
-		map.set(obj, true)
+		map.set(obj, copy)
 	}
 	// for (let key in obj) {
 	// 	if (obj.hasOwnProperty(key)) copy[key] = deepCopy(obj[key], map)
