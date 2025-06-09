6b7fd1cbca4dfe11d1e2c46fbb3e55e7afa71c27
chenzikai <evrytoschen@tencent.com>
2025-06-09 12:28:58 +0800

    feat:generate tree

--- 二叉树/树的遍历/generateTreeFromList.js
+++ 二叉树/树的遍历/generateTreeFromList.js
@@ -1,6 +1,6 @@
 import TreeNode from '../generateTree.js'
 //跟据前序遍历和后序遍历序列构建树 : 不变量为左闭右开
-const buildTree = function (preorder, inorder) {
+const buildTreeV2 = function (preorder, inorder) {
 	if (!preorder.length) return null
 	//分割数组
 	const target = preorder.shift()
@@ -14,8 +14,30 @@ const buildTree = function (preorder, inorder) {
 	const rightPreorder = preorder.slice(index, preorder.length)
 	return new TreeNode(
 		target,
-		buildTree(leftPreorder, leftInorder),
-		buildTree(rightPreorder, rightInorder)
+		buildTreeV2(leftPreorder, leftInorder),
+		buildTreeV2(rightPreorder, rightInorder)
+	)
+}
+
+// 从后序和中序序列构建树，不变量左闭右开
+const buildTree = function (inorder, postorder) {
+	if (!inorder.length || !postorder.length) return null
+	// 后序的最后一个就是跟节点 左右中
+	const target = postorder.pop()
+	// 中序通过后序找到的当前节点被分割为左右  左中右
+	let i = 0
+	for (i; i < inorder.length; i++) {
+		if (inorder[i] === target) break
+	}
+	const inLeft = inorder.slice(0, i)
+	const inRight = inorder.slice(i + 1)
+	const postLeft = postorder.slice(0, i)
+	const postRight = postorder.slice(i)
+
+	return new TreeNode(
+		target,
+		buildTree(inLeft, postLeft),
+		buildTree(inRight, postRight)
 	)
 }
 
