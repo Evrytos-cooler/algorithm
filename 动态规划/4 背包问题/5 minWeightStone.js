543d8f6d67edc158b33a701e9e3266f42600ee86
Evrytos <3174232122@qq.com>
2025-07-09 22:55:27 +0800

    feat: 最后的石头重量

--- 动态规划/4 背包问题/5 minWeightStone.js
+++ 动态规划/4 背包问题/5 minWeightStone.js
@@ -1,6 +1,6 @@
 // 关键思路：将石头分成重量尽量相等的两堆
 // 转换成01背包，大小为sum/2的背包能装的最多的内容就是最靠近相等的其中一堆
-const minWeightStone = arr => {
+var minWeightStone = arr => {
 	const _sum = arr.reduce((prev, cur) => prev + cur, 0)
 	//偏小的那堆
 	const sum = Math.floor(_sum / 2)
@@ -26,3 +26,17 @@ const minWeightStone = arr => {
 	return result
 }
 console.log(minWeightStone([1, 2, 3, 4]))
+
+// 使用一维数组压缩状态，注意大小要从后往前遍历
+var minWeightStone = arr => {
+	// half 是更多的那个
+	const sum = arr.reduce((prev, cur) => prev + cur)
+	const half = Math.floor(sum / 2)
+	const dp = new Array(half + 1).fill(0)
+	for (let i = 0; i < arr.length; i++) {
+		for (let j = half; j >= arr[i]; j--) {
+			dp[j] = Math.max(dp[j], dp[j - arr[i]] + arr[i])
+		}
+	}
+	return sum - 2 * dp[half]
+}
