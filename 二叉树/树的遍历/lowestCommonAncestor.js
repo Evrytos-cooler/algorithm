d30a5ca5551235367fb676d403b7ecc51b85d6dc
Evrytos <3174232122@qq.com>
2025-08-07 23:42:07 +0800

    feat: 最近祖先，传递目标值

--- 二叉树/树的遍历/lowestCommonAncestor.js
+++ 二叉树/树的遍历/lowestCommonAncestor.js
@@ -1,5 +1,5 @@
 // 用后续遍历，如果左边和右边都找到了对应的节点的，root就是目标，否则就传递找到的那个节点
-const lowestCommonAncestor = (root, p, q) => {
+var lowestCommonAncestor = (root, p, q) => {
 	if (!root || !p || !q) return null
 	if (root === p || root === q) return root
 	const left = lowestCommonAncestor(root.left, p, q)
@@ -8,4 +8,16 @@ const lowestCommonAncestor = (root, p, q) => {
 	else {
 		return left ? left : right
 	}
+}
+// 这道题压缩的有点厉害，其实是迭代遍历树，然后看当前节点是不是公共祖先
+
+var lowestCommonAncestor = (root, p, q) => {
+	if (!root) return false
+	if (root === p || root === q) return root // 说明已经找到了一边
+	// 然后开始一边一边的找
+	const left = lowestCommonAncestor(root.left, p, q)
+	const right = lowestCommonAncestor(root.right, p, q)
+	// 如果两边都有说明找到 left 和 right
+	if (left && right) return root
+	return left ? left : right
 }
