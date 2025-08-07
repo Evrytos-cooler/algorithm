704903afb3c060a799523540f1e98774c7219573
chenzikai <evrytoschen@tencent.com>
2025-08-07 13:10:52 +0800

    feat: 完全二叉树的遍历，countNode

--- 二叉树/遍历 + 回溯/countNodes.js
+++ 二叉树/遍历 + 回溯/countNodes.js
@@ -41,17 +41,50 @@ var countNodes = function (root) {
 	const result = Math.pow(2, maxLevel) - 1 - leakCount
 	return result
 }
+
+// 思路是后后续遍历，找到缺的，计算出层级，这样可以省略一部分遍历
+// 什么时候结束遍历： 当前遇到最后一层的节点的时候，说明已经遍历完了所有的缺陷节点
+// 如何知道是最后一层的节点？ 遍历到最后一层的节点时候，会继续 node.left ，此时层级大于最大层级，而且 node 是空
+var countNodesV2 = function (root) {
+	let maxLevel = 0
+	let leakCount = 0
+	let breakFlag = false
+	function traversal(node, level) {
+		if (breakFlag) return
+		if (node) {
+			traversal(node.right, level + 1)
+			traversal(node.left, level + 1)
+			return
+		}
+		// 遇到空节点的时候开始收集
+		if (!node) {
+			// 遇到 n + 1 层的空节点，说明已经记录完所有的 leakCount
+			if (maxLevel !== 0 && level > maxLevel) {
+				breakFlag = true
+			}
+			// 首次遇到空节点，可以开始收集
+			maxLevel = Math.max(maxLevel, level)
+			leakCount++
+		}
+	}
+
+	traversal(root, 1)
+	return 2 ** maxLevel - 1 - leakCount
+}
 // Test case 1: 空树
 const tree1 = null
 console.log(countNodes(tree1)) // Expected: 0
+console.log(countNodesV2(tree1)) // Expected: 0
 
 // Test case 2: 只有根节点
 const tree2 = new TreeNode(1)
 console.log(countNodes(tree2)) // Expected: 1
+console.log(countNodesV2(tree2)) // Expected: 1
 
 // Test case 3: 2层完全二叉树
 const tree3 = new TreeNode(1, new TreeNode(2), new TreeNode(3))
 console.log(countNodes(tree3)) // Expected: 3
+console.log(countNodesV2(tree3)) // Expected: 3
 
 // Test case 4: 3层完全二叉树
 const tree4 = new TreeNode(
@@ -60,7 +93,4 @@ const tree4 = new TreeNode(
 	new TreeNode(3, new TreeNode(6), new TreeNode(7))
 )
 console.log(countNodes(tree4)) // Expected: 7
-
-// Test case 5: 非完全二叉树
-const tree5 = new TreeNode(1, new TreeNode(2, new TreeNode(4), null), new TreeNode(3))
-console.log(countNodes(tree5)) // Expected: 4 (但当前算法会返回3，说明有问题)
+console.log(countNodesV2(tree4)) // Expected: 7
