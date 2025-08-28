0df0881a22a9e0491952d771b0daa02b65c57e98
chenzikai <evrytoschen@tencent.com>
2025-08-28 16:04:13 +0800

    feat: 贪心：买柠檬水 & 最大区域

--- 贪心/lemonadeChange.js
+++ 贪心/lemonadeChange.js
@@ -1,5 +1,6 @@
 // 面额只有 5 10 20 ，柠檬水是 5 元
 // 尽量让5保留的久一点，因为5是最万能的
+// https://leetcode.cn/problems/lemonade-change/description/
 const lemonadeChange = bills => {
 	if (bills[0] !== 5) return false
 	let map = new Map()
@@ -29,5 +30,51 @@ const lemonadeChange = bills => {
 	}
 	return true
 }
+
+const lemonadeChangeV2 = bills => {
+	if (bills[0] !== 5) return false
+	let change = {
+		five: 0,
+		ten: 0,
+		twenty: 0,
+	}
+
+	// bills 是从小到大的
+	for (let i = 0; i < bills.length; i++) {
+		if (bills[i] === 5) {
+			change.five++
+			continue
+		}
+
+		if (bills[i] === 10) {
+			if (change.five < 1) return false
+			else {
+				change.five--
+				change.ten++
+				continue
+			}
+		}
+
+		if (bills[i] === 20) {
+			if (change.ten >= 1 && change.five >= 1) {
+				change.ten--
+				change.five--
+				change.twenty++
+				continue
+			}
+			if (change.five >= 3) {
+				change.five -= 3
+				change.twenty++
+				continue
+			} else {
+				return false
+			}
+		}
+	}
+	return true
+}
+
 console.log(lemonadeChange([5, 5, 10, 10, 20]))
 console.log(lemonadeChange([5, 5, 5, 10, 20]))
+console.log(lemonadeChangeV2([5, 5, 10, 10, 20]))
+console.log(lemonadeChangeV2([5, 5, 5, 10, 20]))
--- 贪心/maxArea.js
+++ 贪心/maxArea.js
@@ -1,12 +1,16 @@
 // 暴力方法就是直接遍历所有情况
 // 用贪心的思路优化剪枝，对于必然变小的情况我们去避免
 // 宽度换高度，同时一样的宽度尽量换更多的高度
+// 所有 (left, right-1)、(left, right-2)、...、(left, left+1) 的组合的面积都不会比 (left, right) 更大，因为：
+// 高度 ≤ height[left]（因为 height[left] 是较矮的）。
+// 宽度 < (right - left)
 const maxArea = function (height) {
 	let left = 0
 	let right = height.length - 1
 	let area = Math.min(height[left], height[right]) * (right - left)
 
 	while (left < right) {
+		// 由于短板效应，我们必须移动短的那个板，才有可能变得更大
 		if (height[left] < height[right]) {
 			left++
 		} else {
